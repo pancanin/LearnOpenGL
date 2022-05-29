b803cd16a02d47ee1ccc7bf4dd973b1465623021
valeri hristov <valericfbg@gmail.com>
2022-05-29 03:58:02 +0300

    vertex shader

--- main.cpp
+++ main.cpp
@@ -13,6 +13,13 @@ void processInput(GLFWwindow* window)
 		glfwSetWindowShouldClose(window, true);
 }
 
+const char* vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
 int main() {
 	glfwInit();
 
@@ -56,6 +63,12 @@ int main() {
 		glGenBuffers(1, &VBO);
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		unsigned int vertexShader;
+		vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+		glCompileShader(vertexShader);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
