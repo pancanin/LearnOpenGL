12925df19f188e2ec0d7d8a9fc9ae28c8196b3a0
quenchaman <valericfbg@gmail.com>
2022-07-30 13:03:49 +0300

    retargeting the solution to 142

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
+#include "Window.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
