5c0a62806fe17a00ef7be27afefe3ed9e3c334d8
quenchaman <valericfbg@gmail.com>
2022-08-05 23:04:53 +0300

    Mixing texture color with positional color

--- shaders/texture_fragment.glsl
+++ shaders/texture_fragment.glsl
@@ -2,10 +2,11 @@
 out vec4 FragColor;
 
 in vec2 TextureCoords;
+in vec4 position;
 
 uniform sampler2D ourAutomaticallyAttachedTexture;
 
 void main()
 {
-    FragColor = texture(ourAutomaticallyAttachedTexture, TextureCoords);
+    FragColor = texture(ourAutomaticallyAttachedTexture, TextureCoords) * (position + vec4(1.0, 1.0, 1.0, 1.0f));
 }
--- shaders/texture_vertex.glsl
+++ shaders/texture_vertex.glsl
@@ -3,9 +3,11 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec2 aTextureCoords;
 
 out vec2 TextureCoords;
+out vec4 position;
 
 void main()
 {
     gl_Position = vec4(aPos, 1.0);
 		TextureCoords = aTextureCoords;
+		position = gl_Position;
 }
