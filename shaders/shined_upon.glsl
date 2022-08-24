08bdbafcd81bb42b0712b3315d46c83f6bfd7a38
quenchaman <valericfbg@gmail.com>
2022-08-25 02:22:33 +0300

    Gourand shading

--- shaders/shined_upon.glsl
+++ shaders/shined_upon.glsl
@@ -1,28 +1,9 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 Normal;
-in vec3 FragWorldPos;
-in vec3 lightPosO;
-
-uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform vec3 lightPos;
-uniform vec3 viewerPos;
+in vec3 outColor;
 
 void main()
 {
-		float ambientStrength = 0.2f;
-		float specularStr = 0.9f;
-		vec3 ambient = objectColor * ambientStrength;
-		vec3 lightDir = normalize(lightPosO - FragWorldPos);
-		float dotP = max(dot(normalize(Normal), lightDir), 0.0f);
-		vec3 diffuse = dotP * lightColor;
-
-		vec3 viewVector = normalize(-FragWorldPos);
-		vec3 reflectDir = reflect(-lightDir, normalize(Normal));
-
-		vec3 specularAngleDiff = pow(max(dot(viewVector, reflectDir), 0.0f), 32) * specularStr * lightColor;
-
-    FragColor = vec4(objectColor * (ambient + diffuse + specularAngleDiff), 1.0f);
+    FragColor = vec4(outColor, 1.0f);
 }
--- shaders/texture_vertex.glsl
+++ shaders/texture_vertex.glsl
@@ -5,6 +5,7 @@ layout (location = 1) in vec3 aNormal;
 out vec3 Normal;
 out vec3 FragWorldPos;
 out vec3 lightPosO;
+out vec3 outColor;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -19,5 +20,19 @@ void main()
 	gl_Position = projection * view * model * vec4(aPos, 1.0);
   FragWorldPos = vec3(view * model * vec4(aPos, 1.0));
   Normal = mat3(transpose(inverse(view * model))) * aNormal;
-  lightPosO = vec3(view * vec4(lightPos, 1.0)); // Transform world-space light position to view-space light position
+  vec3 lightPosView = vec3(view * vec4(lightPos, 1.0)); // Transform world-space light position to view-space light position
+
+	float ambientStrength = 0.2f;
+	float specularStr = 0.9f;
+	vec3 ambient = objectColor * ambientStrength;
+	vec3 lightDir = normalize(lightPosView - FragWorldPos);
+	float dotP = max(dot(normalize(Normal), lightDir), 0.0f);
+	vec3 diffuse = dotP * lightColor;
+
+	vec3 viewVector = normalize(-FragWorldPos);
+	vec3 reflectDir = reflect(-lightDir, normalize(Normal));
+
+	vec3 specularAngleDiff = pow(max(dot(viewVector, reflectDir), 0.0f), 256) * specularStr * lightColor;
+
+  outColor = objectColor * (ambient + diffuse + specularAngleDiff);
 }
